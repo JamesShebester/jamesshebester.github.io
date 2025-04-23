124b8d09012d7726e6967659daed8ce5de239737
JamesShebester <James.Shebester@optimizely.com>
2025-04-23 17:25:19 -0500

    Edge Worker update

---
+++ netlify/edge-functions/inject-message.js
@@ -0,0 +1,22 @@
+export default async (request, context) => {
+    const response = await context.next();
+
+    const newHeaders = new Headers(response.headers);
+    newHeaders.set("content-type", "text/html");
+
+    const rewriter = new HTMLRewriter()
+        .on("body", {
+            element(el) {
+                el.append(`<div style="background: yellow; padding: 1em;">🔥 Injected at the edge!</div>`, {
+                    html: true,
+                });
+            },
+        });
+
+    return rewriter.transform(
+        new Response(response.body, {
+            status: response.status,
+            headers: newHeaders,
+        })
+    );
+};
